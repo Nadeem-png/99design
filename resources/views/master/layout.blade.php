12c6cdd7865fa13bf764a6228f96516a958df081
Nadeem-png <nk27890@gmail.com>
2022-06-02 04:38:32 -0700

    first

--- resources/views/master/layout.blade.php
+++ resources/views/master/layout.blade.php
@@ -21,6 +21,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
 <script src="https://kit.fontawesome.com/9ddc0bba90.js" crossorigin="anonymous"></script>
+
 </head>
     <body>
     <!-- <header class="site-header site-header--static site-header--signed-out">
